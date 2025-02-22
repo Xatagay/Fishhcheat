b9727252307ff4c13e6a50e7323dfba33b1c96a9
Xatagay <m-13091436@moe-dl.edu.my>
2025-02-22 10:25:58 +0800

    Update Sigmacheat.lua

--- Sigmacheat.lua
+++ Sigmacheat.lua
@@ -388,10 +388,9 @@ GB_WeaponMods:AddToggle('InfAmmo', { Text = 'Infinite Ammo', Default = false, To
 GB_WeaponMods:AddToggle('InfCloak', { Text = 'Infinite Cloak', Default = false, Tooltip = 'Infinite cloak for Agent'})
 GB_WeaponMods:AddToggle('InfCharge', { Text = 'Infinite Shield Charge', Default = false, Tooltip = 'Infinite charge for Annihilator shields'}) -- Possibly detected
 GB_WeaponMods:AddToggle('MaxBuildings', { Text = 'Instant LVL 3 Buildings', Default = false, Tooltip = "Mechanic buildings will instantly be lvl 3 once deployed"})
---[[GB_WeaponMods:AddToggle('FirerateChanger', { Text = 'Firerate Modifier', Default = false, Tooltip = 'Modify the firerate of most weapons'})
+GB_WeaponMods:AddToggle('FirerateChanger', { Text = 'Firerate Modifier', Default = false, Tooltip = 'Modify the firerate of most weapons'})
 GB_WeaponMods:AddSlider('FirerateAmount', {Text = 'Firerate', Default = 0.2, Min = 0.1, Max = 1, Rounding = 2, Compact = true})
 -- fakeaxis fight me irl
-]]
 
 Toggles.AlwaysBackstab:OnChanged(function() -- Always Backstab
 	if Toggles.AlwaysBackstab.Value then
@@ -1675,11 +1674,11 @@ end))
 local index -- Firerate changer and Wallbang
 index = hookmetamethod(game, "__index", newcclosure(function(self, key)
     if not Library.Unloaded then
-		--[[if key == "Value" and self:IsA("ValueBase") and not checkcaller() then
+		if key == "Value" and self:IsA("ValueBase") and not checkcaller() then
 			if self.Name:lower():match("firerate") and Toggles.FirerateChanger.Value and not self.Parent:FindFirstChild("Projectile") then
 				return Options.FirerateAmount.Value
 			end
-		end]]
+		end
         if Toggles.Wallbang.Value and key == "Clips" then
             return workspace.Map
         end
